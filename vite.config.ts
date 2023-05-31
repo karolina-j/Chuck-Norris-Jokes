036e31c755a24b4d19d2db5dca04e39c2a39963f
karolina-j <karolina.janygaDev@gmail.com>
2023-05-31 08:59:37 +0200

    Update and add git workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { config } from 'process'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Chuck-Norris-Jokes/",
   plugins: [react()],
-})
+
+});
+
+

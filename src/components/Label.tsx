e0ce3d76c0a2f7dcdf175da9d8f377281557f613
karolina-j <karolina.janygaDev@gmail.com>
2023-05-31 06:50:05 +0200

    Add Label component

---
+++ src/components/Label.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { ReactNode } from 'react';
+
+type Props = {
+  children?: ReactNode;
+};
+
+const Label = ({ children }: Props) => {
+  return <label>{children}</label>;
+};
+
+export { Label };

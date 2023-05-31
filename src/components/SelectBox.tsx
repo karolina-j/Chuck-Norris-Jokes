b176b677a5b087714655986cb3985f6cb47f5f48
karolina-j <karolina.janygaDev@gmail.com>
2023-05-31 06:49:39 +0200

    Add SelectBox component

---
+++ src/components/SelectBox.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { ChangeEvent } from 'react'
+import { Label } from "./Label";
+
+type SelectOption = {
+    label: string
+    value: string
+}
+
+type Props = {
+    value?: string;
+    label?: string;
+    disabled?: boolean;
+    className?: string;
+    options: SelectOption[];
+    onChange: (e: ChangeEvent<HTMLSelectElement>) => void;
+}
+
+
+const SelectBox = ({
+    value,
+    label,
+    disabled,
+    className,
+    options,
+    onChange,
+}: Props) => {
+    const selectBox = (
+        <select
+            className={className}
+            disabled={disabled}
+            onChange={onChange}
+            value={value}
+        >
+            {options.map(({ value, label }) => (
+                <option key={label} value={value}>
+                    {label}
+                </option>
+            ))}
+        </select>
+    )
+
+    const result = label ? (
+        <Label>
+            <div>{label}</div>
+            <div className="select"> {selectBox} </div>
+        </Label>
+    ) : (
+        selectBox
+    )
+
+
+    return <div className="optionBox"> {result} </div>
+}
+
+export default SelectBox
+export type {SelectOption}
